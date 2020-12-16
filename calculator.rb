55071aff074332826f41cfc9789d876ad6b235d5
Phong Lo <phonglo724@gmail.com>
2020-12-16 04:33:03 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,15 @@
-first_number = 1
+first_number = 15
 
-second_number = 2
+second_number = 150
+
+sum = first_number + second_number
+
+difference = second_number - first_number
+
+product = first_number * second_number
+
+quotient = second_number / first_number
+
 
 
 
